eb4d2b91918c32815fd9619ab3c33e5be2474f0b
Martinez Josefina <josefinamartinez239@gmail.com>
2025-04-01 19:06:16 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main() {
+    /*PUNTO 3A*/
     printf("Hola, mundo!\n");
-    return 0;
+
+/*PUNTO 3C: veo el archivo .gitignore, README.md y tp1_1.c. A mi parecer no hay algun archivo que no deba estar*/
+/*PUNTO 3E*/
+ int variable = 30;
+ int *puntero;
+ puntero = &variable;
+
+ printf("Contenido del puntero: %d\n", *puntero);
+
+ printf("Direccion de memoria almacenada por el puntero: %p\n", puntero);
+
+ printf("Direccion de memoria de la variable: %p\n", &variable);
+
+ printf("Direccion de memoria del puntero: %p\n", &puntero);
+
+ printf("Tamanio de la variable en memoria: %zu bytes\n", sizeof(variable));
+
 }
